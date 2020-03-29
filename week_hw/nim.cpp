e9b33e119af9d3d9bedda3a03875c73b806d783c
Oliver Chen <oliverchen5285@gmail.com>
2020-03-29 10:01:00 -0700

    hw for this week

---
+++ week_hw/nim.cpp
@@ -0,0 +1,6 @@
+//write an interactive nim game where computer is an option (1 pile of n stones, take [1, 2, 3] stones)
+
+//function getstonesleft() returns how many stones, function makemove() returns take how many stones
+//can be expanded to the true nim game (multiple piles, take any amount)
+
+//play othello
---
+++ week_hw/oNsort.cpp
@@ -0,0 +1,1 @@
+write function that sorts list of ascii characters in O(N) time and O(M) memory
