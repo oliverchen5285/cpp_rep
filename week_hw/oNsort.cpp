ff6764e8af183142fa1fb58e34f5d8dfce9a1759
Oliver Chen <oliverchen5285@gmail.com>
2020-04-01 20:15:40 -0700

    added oNsort.cpp

--- week_hw/oNsort.cpp
+++ week_hw/oNsort.cpp
@@ -1,1 +1,31 @@
-write function that sorts list of ascii characters in O(N) time and O(M) memory
+//write function that sorts list of ascii characters in O(N) time and O(M) memory
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <map>
+using namespace std;
+
+int main(){
+  string in;
+  cin >> in;
+
+  map<char, int> dict;
+
+  for(int i = 0; i < in.length(); ++i){
+    ++dict[in[i]];
+  }
+  for(auto &i : dict){
+    for(int j = 0; j < i.second; ++j){
+      cout << i.first;
+    }
+  }
+  cout << endl;
+}
+
+
+
+
+
