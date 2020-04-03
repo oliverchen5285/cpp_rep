8dabe5627b03adb47491034841b513f6df6d2f59
Oliver Chen <oliverchen5285@gmail.com>
2020-04-02 23:55:03 -0700

    created nim function that returns proper integer move using dynamic programming

--- week_hw/nim.cpp
+++ week_hw/nim.cpp
@@ -1,4 +1,70 @@
 //write an interactive nim game where computer is an option (1 pile of n stones, take [1, 2, 3] stones)
+#include <iostream>
+#include <string>
+#include <vector>
+#include <set>
+#include <queue>
+#include <utility>
+#include <map>
+using namespace std;
+
+/*
+int move(int stoneindex, int maxmoves, vector<int> &winloss){
+  //2 = undecided, 1 = win, 0 = loss (if start turn with stonenum)
+  bool win = false;
+  for(int i = stoneindex; i >= stoneindex - maxmoves; --i){
+    if(winloss[stoneindex] == 0){
+      win = true;
+    }
+  }
+  
+}
+*/
+
+int move2(int stoneindex, int maxmoves){
+  vector<bool> winloss2(stoneindex + 1, false);
+  winloss2[0] = false;
+  for(int i = 1; i <= stoneindex; ++i){
+    for(int j = 1; j <= maxmoves; ++j){
+      if(i - j >= 0){
+        if(winloss2[i - j] == false){
+          winloss2[i] = true;
+        }
+      }
+    }
+  }
+  for(int i = 0; i < winloss2.size(); ++i){
+    if(winloss2[i] == false){
+      cout << 0;
+    }
+    else{
+      cout << 1;
+    }
+  }
+  cout << endl;
+  if(winloss2[stoneindex] == true){
+    for(int i = stoneindex; i >= stoneindex - maxmoves; --i){
+      if(winloss2[i] == false){
+        return stoneindex - i;
+      }
+    }
+  }
+  
+  return maxmoves;
+  
+}
+/*
+bool play(int stonenum, int maxmoves){
+  //returns true if player 1 wins, false if player 2 wins
+  
+}
+*/
+
+int main(){
+  for(int i = 1; i < 11; ++i){
+    cout << i << ": " <<  move2(i, 3) << endl;
+  }
+}
 
 //function getstonesleft() returns how many stones, function makemove() returns take how many stones
 //can be expanded to the true nim game (multiple piles, take any amount)
