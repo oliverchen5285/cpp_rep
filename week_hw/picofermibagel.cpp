9f46871c94b2282d2ed94680dc1d40c4a33dc53a
Oliver Chen <oliverchen5285@gmail.com>
2020-06-28 08:03:30 -0700

    I rewrote everything and changed my approach a bit but still got stuck on picking random numbers for a given clue because choosing the best digit to pick every time will likely result in some infinite loop AND choosing different numbers every time won't allow me to make progress if my secret number has digits spread out everywhere. (I think I need to find a way to sometimes include the "best digits" in my guess for some digits but pick new ones for some other digits). For picking digits, I may have to use recursion to test out possible guesses because the "best" or sorted list will sometimes allow digits to have no possible values (if digits before it pick all its remaining digits). Basically, isntead of grouping digits into sets, I put them all into the same vector so I could sort them based on availability and probability (represented by enum values). I thought that would solve my previous issues with not being able to pick which digits to guess BUT now I face a similar problem because I would have to randomly pick some digits to have a random value (most likely after most digits have already been used) because guessing completely new or the complete same digits will not help me. BUT then trying to guess "non-optimal" digits might result in some digits not being able to have values...? BECAUSE I am not choosing the digits that have the best chance (but then that's true for the "best" digits too...). I might be able to solve this just by continuously trying to create a guess (and then change that guess by picking lower digit values for digits and reordering digits). I don't think the time complexity will be too bad, but I think it would still be possible for certain clues to create an area where my guessing function would keep picking useless guesses. Only now did I realize I didn't commit (or email...) soooo

--- week_hw/picofermibagel.cpp
+++ week_hw/picofermibagel.cpp
@@ -3,9 +3,11 @@
 #include <vector>
 #include <utility>
 #include <cstdlib> //rand()
+#include <map> //unordered_map included?
+#include <algorithm>
 using namespace std;
 
-string genNum(const int &digits){ //no repeating digits
+string genNum(const int digits){ //no repeating digits
   string ans = "";
   vector<int> nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
   int bound = 9;
@@ -81,7 +83,7 @@ int game(int digits, string mode){
   return count;
 }
 
-
+/* previous class
 class Comp{
 private:
   int digits_num;
@@ -181,9 +183,176 @@ public:
   
   
 };
+ */
 
+class Comp1_c{
+private:
+    int digits;
+    pair<int, int> clue;
+    
+public:
+    Comp1_c(int new_digits){ //constructor
+        digits = new_digits;
+    }
+    
+    string make_guess(){ //random guesses with potential repeats
+        string ans = ""; //does the same thing as gen_num()
+        vector<int> nums = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        int bound = 9;
+        for(int i = 0; i < digits; ++i){
+          int cur = rand()%(bound+1); //maybe create helper function to generate (moreso) (pseudo) random number in range (0, 9)
+          ans += to_string(nums[cur]);
+          swap(nums[cur], nums[bound]);
+          bound -= 1;
+        }
+        return ans;
+    }
+    void get_clue(pair<int, int> new_clue){ //function to take input of clue
+        clue = new_clue; //other than this do absolutely n o t h i n g
+    }
+}
 
+class Comp2_c{
+private:
+    int digits;
+    pair<int, int> clue;
+    
+    vector<string> pos_guesses;
+    
+public:
+    Comp2_c(int new_digits){ //constructor
+        digits = new_digits;
+        
+        //generate all possible guesses (once in constructor) -- I do this instead of repeatedly trying random guesses (until I get a different one) because iterating over everything once is probably better than every time...
+        for(int i = 0; i < digits; ++i){ //iterate over every digit
+            string temp_guess = "";
+            for(int j = 0; j < 10; ++j){  //iterate over every possible digit value
+                temp_guess += to_string(j);
+            }
+            pos_guesses.push_back(temp_guess);
+        }
+        //remove guesses with duplicate digits (invalid)
+        for(int i = 0; i < pos_guesses.size(); ++i){
+            vector<bool> vis_digits(10, false); //set all 10 digits to unvisited
+            
+            for(const auto &temp_digit: pos_guesses[i]){ //iterate through digits in every guess
+                if(vis_digits[(int)temp_digit - '0']){ //if current digit already visited
+                    pos_guesses.erase(pos_guesses.begin() + i); //erase current guess from vector
+                    break;
+                }
+                vis_digits[(int)temp_digit - '0'] = true; //set current digit to visited
+            }
+        }
+    }
+    
+    string make_guess(){ //random guesses without repeats
+        int index = rand()%(pos_guesses.size()); //maybe replace this with self made random function
+        
+        string ans = pos_guesses[index];
+        pos_guesses.erase(pos_guesses.begin() + index);
+        
+        return ans;
+    }
+    void get_clue(pair<int, int> new_clue){ //function to take input of clue
+        clue = new_clue; //other than this do absolutely n o t h i n g
+    }
+}
 
+enum Possibility_e{ //Just wanted to experiment with enums (instead of boolean states, in case I had to add more), allows me to name states instead of ints? idrk why I did this
+    No, Pos, Prob, Used //No = no chance; Pos = possible, idk; Prob = clue gave fermi (guess from Probs if possible?), Used = used in previous digit (DO NOT CHOOSE)
+};
+
+class Comp3_c{
+private:
+    int digits;
+    pair<int, int> clue;
+    
+    string guess;
+    
+    vector<pair<int, vector<Possibility_e>>> digit_probs;
+    
+    unordered_map<Possibility_e, int> pos_priority_map = {{No, 0}, {Pos, 1}, {Prob, 2}, {Used, 0}}; //made map just in case
+    //in testing I'm pretty sure I could compare enum values with '>' but not add them with + (when I print them, they are represented as integers?)
+    
+    bool compare_digit_prob(pair<int, vector<Possibility_e>> x, pair<int, vector<Possibility_e>> y, int cur_digit){ //compares 2 elements of digit_probs by specific digit
+        if(x.second[cur_digit] == y.second[cur_digit]){
+            return x.first > y.first;
+        }
+        return pos_priority_map[x.second[cur_digit]] > pos_priority_map[y.second[cur_digit]]; //Maybe possible for all digits to be 0... (if previous digits chose all possible digits of current digit)
+    }
+    
+    bool compare_digit_occur(pair<int, int> x, pair<int, int> y){ //helper function to sort pairs by second value used in sort_columns()
+        return x.second > y.second; //maybe have to swap this?
+    }
+    
+    vector<int> sort_columns(){ //returns vector based on which columns should be picked first (which ones have less options available...?)
+        vector<pair<int, int>> ans;
+        for(int i = 0; i < digits; ++i){
+            ans.push_back({i, 0});
+        }
+        for(int i = 0; i < digit_probs.size(); ++i){ //iterate through every possible digit value
+            for(int j = 0; j < digit_probs[i].second.size(); ++j){ //iterate through every digit
+                ans[j] += min(1, pos_priority_map[digits_probs[i].second[j]]); //if adding anything, add 1 or 0
+            }
+        }
+        sort(ans.begin(), ans.end(), compare_digit_occur);
+    }
+    
+    
+public:
+    Comp3_c(int new_digits){ //constructor
+        digits = new_digits;
+        
+        //maybe I can do this in initialization, but not sure about digits being passed in before digit_probs created?
+        for(int i = 0; i < 10; ++i){ //iterate through every possible digit value
+            vector<Possibility_e> temp_probs(digits, Pos); //initiate every digit's probability vec as filled with Pos (all possible)
+            digit_probs.push_back({i, temp_probs}); //push back pair of digit value and vector
+        }
+    }
+    
+    string make_guess(){ //I still can't find a way to pick random numbers because picking the best numbers every time will likely get be stuck and picking different numbers every time won't really be using the clues
+        string cur_guess = ""; //JUST ASSUME just ASSUME that this works SOMEOHOW
+        
+        
+        for(int i = 0; i < digits; ++i){ //ok dang it dang it dang it -- after picking one element from one digit column, the other columns are affected and their order might change (and if I follow the initial order some columns may be empty)
+            vector<int> digit_order = sort_solumns(); //does choosing the "best" set potentially result in infinite loop? (if best set never gives new limiting information) -- or "best" set never gives too much information because some digits give few info <- could make another enum value for "picked before in PREVIOUS guess"
+            //sort each column to find best digit value
+            //cur_guess +=
+            //then I would have to temporarily update digit_probs so all of the digits that use the current one are marked as used
+        }
+        guess = cur_guess;
+        return guess;
+    }
+    
+    void get_clue(pair<int, int> new_clue){
+        clue = new_clue;
+        if(clue.second == digits){ //all fermi
+            return;
+        }
+        if(clue.first == 0 && clue.second == 0){ //bagel
+            for(const auto &cur_digit: guess){ //cur_digit is a char
+                int cur_digit_int = (int)cur_digit - '0';
+                fill(digit_probs[cur_digit_int].begin(), digit_probs[cur_digit_int].end(), '0');
+            }
+        }
+        else if(clue.second > 0){ //if any of them are in the correct position
+            for(int i = 0; i < guess.length(); ++i){ //cur_digit is a char
+                int cur_digit_int = (int)guess[i] - '0';
+                fill(digit_probs[cur_digit_int].second.begin(), digit_probs[cur_digit_int].second.end(), '1');
+                digit_probs[cur_digit_int].second[i] == '2';
+            }
+        }
+        else{
+            for(int i = 0; i < guess.length(); ++i){ //cur_digit is a char
+                int cur_digit_int = (int)guess[i] - '0';
+                fill(digit_probs[cur_digit_int].second.begin(), digit_probs[cur_digit_int].second.end(), '0');
+                digit_probs[cur_digit_int].second[i] == '1';
+            }
+        }
+        
+    }
+    
+}
 
 int main(){
   int digits;
